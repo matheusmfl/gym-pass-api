64412b28b4e2f894fa64b28183aaf48d9227aade
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-01-28 14:04:53 -0300

    Generate password hash using bcrypt

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma"
 import { FastifyReply, FastifyRequest } from "fastify"
+import { hash } from 'bcryptjs'
 import { z } from "zod"
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
@@ -10,13 +11,17 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
     password: z.string().min(6)
   })
 
+
+
   const { name, email, password } = registerBodySchema.parse(req.body)
+
+  const password_hash = await hash(password, 6)
 
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash
     }
   })
 
