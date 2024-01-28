6213901e83e4fc65232a6dc91011c81176cb0fda
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-01-28 14:52:03 -0300

    add first use-case, register

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
-import { prisma } from "@/lib/prisma"
 import { FastifyReply, FastifyRequest } from "fastify"
-import { hash } from 'bcryptjs'
 import { z } from "zod"
+import { registerUseCase } from "@/use-cases/register"
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
 
@@ -15,23 +14,13 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: { email: email }
-  })
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({
+      name, email, password
+    })
+  } catch (error) {
     return res.status(409).send()
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash
-    }
-  })
 
   return res.status(201).send()
 
